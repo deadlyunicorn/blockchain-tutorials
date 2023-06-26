1d9e2405a4bd7a4a1f3e929ef413374e24000705
deadlyunicorn <retroalex1008@gmail.com>
2023-06-26 19:21:16 +0300

    importing in solidity

--- nft-first/contracts/Box.sol
+++ nft-first/contracts/Box.sol
@@ -2,12 +2,24 @@
 
 pragma solidity ^0.8.0;
 
+
+import "./Auth.sol";
+
 contract Box{
     uint256 private _value;
+    Auth private _auth; //Auth is a contract object.
 
     event ValueChanged(uint256 value);
 
+    constructor(){
+        _auth = new Auth(msg.sender); //the deployer gets auth
+    }
+
     function store(uint256 value) public{
+        
+        //Only admin can store values.
+        require(_auth.isAdministrator((msg.sender)),"Unautorized");
+        
         _value=value;
         emit ValueChanged(value);
     }
